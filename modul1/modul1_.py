06ebdcb531220637a3ebe65fb7f9c16fe34b3c95
BPiescu <milosescu_diana@yahoo.com>
2021-02-17 21:35:57 +0200

    modul 1

---
+++ modul1/modul1_.py
@@ -0,0 +1,11 @@
+print('hello world')
+print(print)
+print(3)
+
+print ('My name is:','Diana')
+print('my name is:','Diana','1',sep='_',end='' )
+print()
+
+print ('*')
+print ('***')
+print('*****')
